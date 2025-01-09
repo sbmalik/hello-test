c40583b2ff359134060d1926e4a1fa66232a8410
sbmalik <malik@sigrow.com>
2025-01-09 11:15:20 +0500

    Initial commit with Hello World PyQt app

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt6.QtCore import Qt
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Hello World")
+        self.setFixedSize(300, 150)
+
+        # Create and center the label
+        label = QLabel("Hello, World!", self)
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setCentralWidget(label)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
